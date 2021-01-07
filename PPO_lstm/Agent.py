284d58224b976018d52c87dc63e744cd449a5a7d
hojunkim13 <73092634+hojunkim13@users.noreply.github.com>
2021-01-07 17:36:30 +0900

    Add PPO-lstm

--- PPO_lstm/Agent.py
+++ PPO_lstm/Agent.py
@@ -5,25 +5,26 @@ import torch.nn.functional as F
 
 class Agent:
     def __init__(self, state_dim, action_dim, lr, gamma, lmbda, epsilon,
-                 time_step, K_epochs,):
+                 buffer_size, batch_size, K_epochs,):
         self.Net = ActorCritic(state_dim, action_dim)
-        self.optimizer = torch.optim.Adam(self.Net.parameters(), lr = lr, )
-        torch.nn.utils.clip_grad_norm_(self.Net.parameters(), 5.0)
+        self.optimizer = torch.optim.Adam(self.Net.parameters(), lr = lr)
         self.gamma = gamma
         self.lmbda = lmbda
         self.epsilon = epsilon
         self.state_dim = state_dim
         self.action_dim = action_dim
-
-        self.time_step = time_step
+        self.buffer_size = buffer_size
+        self.batch_size = batch_size
         self.K_epochs = K_epochs
 
-        self.S = np.zeros((time_step,)+ state_dim, dtype = 'float')
-        self.A = np.zeros((time_step, action_dim))
-        self.R = np.zeros((time_step, 1), dtype = 'float')
-        self.S_ = np.zeros((time_step,)+ state_dim, dtype = 'float')
-        self.D = np.zeros((time_step, 1), dtype = 'bool')
-        self.P = np.zeros((time_step, 1), dtype = 'float')
+        self.S = np.zeros((buffer_size,)+ state_dim, dtype = 'float')
+        self.A = np.zeros((buffer_size, action_dim))
+        self.R = np.zeros((buffer_size, 1), dtype = 'float')
+        self.S_ = np.zeros((buffer_size,)+ state_dim, dtype = 'float')
+        self.D = np.zeros((buffer_size, 1), dtype = 'bool')
+        self.P = np.zeros((buffer_size, 1), dtype = 'float')
+        self.H_in = np.zeros((buffer_size, 1), dtype = 'float')
+        self.H_out = np.zeros((buffer_size, 1), dtype = 'float')
         self.mntr = 0
         
     
@@ -39,7 +40,7 @@ class Agent:
         log_prob = log_prob.item()
         return action, log_prob
 
-    def store(self, s, a, r, s_, d, a_prob):
+    def store(self, s, a, a_prob, r, s_, d, h_in, h_out):
         idx = self.mntr
 
         self.S[idx] = s
@@ -48,6 +49,8 @@ class Agent:
         self.S_[idx] = s_
         self.D[idx] = d
         self.P[idx] = a_prob
+        self.H_in[idx] = h_in
+        self.H_out[idx] = h_out
         self.mntr += 1
 
     def get_advantage(self, S, R, S_, D):
--- PPO_lstm/Network.py
+++ PPO_lstm/Network.py
@@ -6,48 +6,31 @@ class ActorCritic(nn.Module):
     def __init__(self, state_dim, action_dim):
         super(ActorCritic, self).__init__()
         #96
-        self.ConvNet = nn.Sequential(nn.Conv2d(4, 16, 4, 2, 1),
-                                   nn.BatchNorm2d(16),
-                                   nn.ReLU(),
-                                   #48
-                                   nn.Conv2d(16, 32, 4, 2, 1),
-                                   nn.BatchNorm2d(32),
-                                   nn.ReLU(),
-                                   #24
-                                   nn.Conv2d(32, 64, 4, 2, 1),
-                                   nn.BatchNorm2d(64),
-                                   nn.ReLU(),
-                                   #12
-                                   nn.Conv2d(64, 128, 4, 4, 0),
-                                   nn.BatchNorm2d(128),
-                                   nn.ReLU(),
-                                   # 3 * 3
-                                   nn.Conv2d(128, 256, 3, 1, 0),
-                                   nn.ReLU(),
-                                   nn.Flatten(),
-                                   )
+        self.fc1 = nn.Sequential(nn.Linear(state_dim, 64),
+                                 nn.ReLU()
+        )
+        self.lstm = nn.LSTM(64, 32)
+                                
 
         self.fc = nn.Sequential(nn.Linear(256, 100),
                                    nn.ReLU())
 
-        self.alpha = nn.Sequential(nn.Linear(100,3),
-                                   nn.Softplus())
-
-        self.beta = nn.Sequential(nn.Linear(100,3),
-                                   nn.Softplus())                                   
-
-        self.critic = nn.Sequential(nn.Linear(256, 100),
-                                    nn.ReLU(),
-                                    nn.Linear(100,1))
-
+        self.probs = nn.Sequential(nn.Linear(32, action_dim),
+                                   nn.Softmax(dim = -1))
+                                       
+        
+        self.critic = nn.Linear(32, 1)
+                                    
         self.cuda()
 
-    def forward(self, state):
-        feature = self.ConvNet(state)
-        value = self.critic(feature)
+    def forward(self, state, hidden):
+        x = self.fc1(state)
+        x = x.view(-1, 1, 64)
+        x, lstm_hidden = self.lstm(x, hidden)
+        value = self.critic(x)
+        probs = self.probs(x)
+        
+        return probs, value, lstm_hidden
 
-        x = self.fc(feature)
-        alpha = self.alpha(x) + 1
-        beta = self.beta(x) + 1
-        return (alpha, beta), value
+        
 
