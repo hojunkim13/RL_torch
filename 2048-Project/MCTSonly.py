631995ca7656403042db7494d5e1a0081b3a9602
hojunkim <hojunkim13@gmail.com>
2021-04-20 10:11:29 +0900

    Fix error

--- 2048-Project/MCTSonly.py
+++ 2048-Project/MCTSonly.py
@@ -3,12 +3,10 @@ from Environment.DosEnv import _2048
 from Environment.Utils import *
 import time
 from threading import Thread
-from threading import Lock
 
-lock = Lock()
 
-n_sim = 400
-n_episode = 1
+n_sim = 200
+n_episode = 5
 
 env_name = "2048"
 env = _2048()
@@ -24,7 +22,6 @@ class MCTS:
         self.root_grid = grid
         self.values = np.zeros(4)
         
-
     def slmulation(self):        
         for first_action in range(4):
             start_grid = move_grid(self.root_grid, first_action)
@@ -32,7 +29,7 @@ class MCTS:
                 continue
             for sim in range(self.n_sim // 4):
                 value = 0
-                grid = start_grid
+                grid = move_grid(self.root_grid, first_action)
                 while not isEnd(grid):
                     action = np.random.randint(0,4)
                     grid = move_grid(grid, action)
@@ -46,15 +43,15 @@ class MCTS:
         if np.array_equal(start_grid, self.root_grid):
             return
         for _ in range(self.n_sim // 4):
-            grid = start_grid
+            grid = move_grid(self.root_grid, first_action)
             value = 0
             while not isEnd(grid):
                 action = np.random.randint(0,4)
                 grid = move_grid(grid, action)
                 value += 1
-        lock.acquire()
-        self.values[first_action] += value        
-        lock.release()
+            
+            self.values[first_action] += value        
+        
         
 
     def simulWithThread(self):
@@ -65,25 +62,26 @@ class MCTS:
             threads.append(worker)
         [worker.join() for worker in threads]        
         return np.argmax(self.values)
+       
 
 def main():
-    start_time = time.time()
     mcts = MCTS(n_sim)
     score_list = []
     for e in range(n_episode):
+        start_time = time.time()
         done = False
         grid = env.reset()
         while not done:
             mcts.setRootGrid(grid)
-            action = mcts.slmulation()
-            #action = mcts.simulWithThread()
-            grid, _, done, info = env.step(action, True)
+            #action = mcts.slmulation()
+            action = mcts.simulWithThread()
+            grid, _, done, info = env.step(action, False)
         score_list.append(info)
         average_score = np.mean(score_list[-100:])        
         spending_time = time.time() - start_time
         print(f"Episode : {e+1} / {n_episode}, Max Tile : {info}, Average: {average_score:.1f}")
         print(f"SPENDING TIME : {spending_time:.1f} Sec")
-        time.sleep(3)
+        #time.sleep(3)
 
 if __name__ == "__main__":
     main()
